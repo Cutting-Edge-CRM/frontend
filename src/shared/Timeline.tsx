5db6f07a92c11c46acf846122b9c45010b316a5c
Nick Weisberg <nickweisberg@nicks-mbp.gateway.sasktel.net>
2023-01-22 14:46:21 -0600

    timeline component idea

---
+++ src/shared/Timeline.tsx
@@ -0,0 +1,64 @@
+import { Card, List, ListItem, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+
+
+function Timeline(props: any) {
+    const [rows, setRows] = useState([] as any);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (props.client) {
+
+            // client created
+            // quotes created
+            // quotes sent
+            // quotes approved
+            // jobs created
+            // invoices created
+            // invoices sent
+            // deposits on all quotes
+            // payments on all invoices
+
+        } else if (props.quote) {
+
+            // quotes created
+            // quotes sent
+            // quotes approved
+            // deposits on all quotes
+
+
+        } else if (props.invoice) {
+
+            // deposit on connected quotes
+            // invoices created
+            // invoices sent
+            // payments on all invoices
+
+        }
+    })
+  
+    if (error) {
+    return (<Typography>{error}</Typography>);
+    }
+    if (!isLoaded) {
+    return (<Typography>Loading...</Typography>);
+    }
+
+    return (
+        <Card>
+            <Typography>Timeline</Typography>
+            <List>
+                {
+                    rows.map((event: any) => (
+                        <ListItem key={event.id}>
+
+                        </ListItem>
+                    ))
+                }
+            </List>
+        </Card>
+    )
+}
+
+export default Timeline;
